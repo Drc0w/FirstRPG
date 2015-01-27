3ec5b262965edc6bf40ea5cd1ca77df8425348e2
perris_p <perris_p@epita.fr>
2015-01-27 18:48:39 +0100

    Possibilité d'afficher le statut du combat avec les informations sur chaque joueur. Ajout également d'une phrase d'introduction

--- src/fight/fight.cpp
+++ src/fight/fight.cpp
@@ -2,15 +2,30 @@
 
 using namespace std;
 
+void PrintInformations(const Personnage *main, const Personnage *ennemy)
+{
+	cout << main->Name() << " is fighting agaisnt " << ennemy->Name() << endl;
+	Weapon *mainw = main->GetWeapon();
+	cout << main->Name() << " uses " << mainw->Name() << " and deals " << mainw->Damages() << endl;
+	Weapon *ennw = ennemy->GetWeapon();
+	cout << ennemy->Name() << "  uses " << ennw->Name() << " and deals " << ennw->Damages() << endl;
+	cout << main->Name() << " has " << main->RemainingLife() << "HP left" << endl;
+	cout << ennemy->Name() << "has " << ennemy->RemainingLife() << "HP left" << endl;
+}
+
 void Fight(Personnage *main, Personnage *ennemy)
 {
 	system("clear");
+	cout << "Un " << ennemy->Name() << " apparait !" << endl;
+	cout << main->Name() << " en avant !" << endl;
 	do
 	{
 		if (main->IsAlive())
 		{
+			cout << endl;
 			cout << "What do you wanna do?" << endl;
 			cout << "1. Attack" << endl;
+			cout << "2. Show info" << endl;
 			int answer;
 			cin >> answer;
 			bool ctinue = false;
@@ -21,6 +36,11 @@ void Fight(Personnage *main, Personnage *ennemy)
 					case 1:
 						system("clear");
 						main->Attack(ennemy);
+						break;
+
+					case 2:
+						system("clear");
+						PrintInformations(main, ennemy);
 						break;
 
 					default:
