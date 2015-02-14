e8baf604ee0da16a03bf27c466cc0d6b317469a6
JeKoCorp <JeKoCorp@users.noreply.github.com>
2015-02-14 01:14:11 +0100

    Ajout d'un menu d'accueil

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,43 @@ Personnage* CreateMainCharacter()
 	return mainChar;
 }
 
+int menu()
+{
+	ClearConsole();
+	cout << "+--------------------+" << endl;
+   	cout << "|   First RPG Beta   |" << endl;
+    	cout << "|   --------------   |" << endl;
+    	cout << "|     By  Dr_c0w     |" << endl;
+    	cout << "+--------------------+" << endl;
+	cout << "Welcome, what's next : " << endl;
+	cout << "1)  Start new game" << endl;
+    	cout << "2)  Load saved game" << endl;
+    	cout << "3)  Tutorial" << endl;
+    	cout << "4)  Quit" << endl;
+    	int choice;
+	cin >> choice;
+    	return choice;
+}
+
 int main()
 {
-	Personnage *me = CreateMainCharacter();
-	FightManager(me);
-	delete me;
-	return 0;
+	Personnage *me;
+    	switch(menu())
+    	{
+        	case 1:
+        	    me = CreateMainCharacter();
+		    FightManager(me);
+		    delete me;
+        	    break;
+        	case 2:
+        	    cout << "New feature soon..." << endl;
+        	    break;
+        	case 3:
+        	    cout << "New feature soon..." << endl;
+        	    break;
+        	case 4:
+        	    cout << "Hope you had a great time, see you soon ;-)" << endl;
+        	    break;
+    	}
+    	return 0;
 }
